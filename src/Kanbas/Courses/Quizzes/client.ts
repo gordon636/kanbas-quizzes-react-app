bc91f268c4fb5c9fc7010ab431a436f98a30621b
gordon636 <Gordon@MacBook-Pro.local>
2024-04-21 23:14:56 -0400

    axios

--- src/Kanbas/Courses/Quizzes/client.ts
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-axios.defaults.withCredentials = true;
-
 import { IQuiz } from "./quizzesReducer";
+
+axios.defaults.withCredentials = true;
 
 const API_BASE = process.env.REACT_APP_BASE_API_URL;
 const COURSES_API = `${API_BASE}/api/courses`;
